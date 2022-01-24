4db57a4e43832813035c5e1d9e17650981e4e87a
Grant Royseth <groyseth@gmail.com>
2022-01-24 15:13:00 -0600

    finale commit

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,3 @@
-// const express = require('express');
-
-// // Import our modular routers for /tips and /feedback
-// const noteRouter = require('./notes');
-// // const feedbackRouter = require('./feedback');
-
-// const app = express();
-
-// app.use('/notes', noteRouter);
-// // app.use('/feedback', feedbackRouter);
-
-// module.exports = app;
-
 const router = require('express').Router();
 const apiRoutes = require('./notes.js')
 
