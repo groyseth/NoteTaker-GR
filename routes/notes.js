e09234cdfda73d5675708f4811544b20f42539ab
Grant Royseth <groyseth@gmail.com>
2022-01-24 14:29:36 -0600

    finished delete

--- routes/notes.js
+++ routes/notes.js
@@ -1,64 +1,42 @@
 const router = require('express').Router();
 const db = require("../db/db.json");
-// const noter = require('./index')
+const filename = "./db/db.json"
 const { readFromFile, readAndAppend } = require('../helper/jsUtils');
 const uuid = require('../helper/uuid');
-// router.get('/notes', (req, res) => {
-//     res.json(db)
-// })
+const fs = require('fs')
 
-// router.post('/notes', (req, res) => {
-//     console.log("seperating our concerns")
-//     res.send("cats")
-// })
+
+
 router.delete('/notes/:id' , (req, res) => {
-    console.info(`${req.method}`);
-    // const userIndex = getUserIndex(req.params.userId)
-    console.log(req.params);
-    // var user = req.body;
-    // delete user;
-    if(req.body){
-        delete {db}
-    }
-    // var username = req.params;
-    // readFromFile('./db/db.json').then((data) =>  delete res.json(JSON.parse(data)));
+   const deleteNotes = req.params.id;
 
-//     const { title, text, id } = req.body;
-
-//     if (req.body) {
-//         const note = {
-//             title,
-//             text,
-//             id,
-//         };
-
-
-
-        
-
-// console.log(note);
-        // readAndAppend(note);
-        // .then((data)res.json(JSON.parse(data)));
-    //     res.json('delete');
-    // //     // readFromFile('./db/db.json').then((data) => res.json(JSON.remove(data)));
-    // // } else {
-    // //     res.error('Error with adding note');
-    // // }
+fs.readFile(filename, 'utf8', function(err, data){
     
+        var notes = JSON.parse(data);
+        var filterNotes = notes.filter(note => note.id !== deleteNotes)
+        console.log(filterNotes);
 
-})
+        fs.writeFile(filename, JSON.stringify(filterNotes), (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            res.json(req.body);
+        })
+    });
+});
+
 
 router.get('/notes', (req, res) => {
-    // console.info(`${req.method} request received for notes`);
+
     console.log(`${req.method}`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile(filename).then((data) => res.json(JSON.parse(data)));
     console.log(req.params);
 
 });
 
 router.post('/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
-    // console.log(req.body);
 
     const { title, text } = req.body;
 
@@ -69,7 +47,7 @@ router.post('/notes', (req, res) => {
             id: uuid(),
         };
 
-        readAndAppend(note, './db/db.json');
+        readAndAppend(note, filename);
         res.json(`note added`);
 
     } else {
@@ -77,53 +55,9 @@ router.post('/notes', (req, res) => {
     }
    
 
-    //     readAndAppend(note, './db/db.json');
-
-    //     const response = {
-    //       status: 'success',
-    //       body: note,
-    //     };
-
-    //     res.json(response);
-    //   } else {
-    //     res.json('Error in posting feedback');
-    //   }
 
 
 });
-// var id = uuid();
 
-// router.delete('/notes/id' , (req, res) => {
-//     // console.info(`${req.method} request received for notes`);
-//     // console.log(`${res.note_id}`);
-//     console.log(`${req.note_id}`);
-    
-//     console.info(req.body);
-     
-//     // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-// console.log( );
-// // });
-// router.delete('/notes/:'+`${id}`, (req, res) => {
-//     // console.info(`${req.method} request received to add a note`);
-    
-//     // console.log("delete: req.body: " + JSON.stringify(req.body));
-//     console.log(`${req.method}`);
-//     // res.send(req.body);
-//     // const { title, text } = req.body;
-// // return  './db/db.json'. id;
-//     // if (req.body) {
-//     //     var note = {
-//     //         title,
-//     //         text,
-//     //         id: uuid(),
-//     //     };
-//     //     console.log(note.id);
-//     //     readAndAppend(note, './db/db.json');
-//     //     res.json(`note added`);
-
-//     // } else {
-//     //     res.error('Error with adding note');
-    
-// })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 // const fs = require('fs');
-const db = require("./db/db.json")
+const filename = require("./db/db.json")
 const util = require('util');
 // const api = require('./routes/index')
 
